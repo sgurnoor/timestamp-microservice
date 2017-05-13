eb1d1f501541c44fe160136425f37d81d83836e9
Gurnoor Singh <gurnoor@umich.edu>
2017-05-13 15:16:03 +0000

    Completed timestamp project

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-var express = require("express")
-var app = express()
+var express = require("express");
+var moment = require("moment");
+var path = require("path");
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/timestamp', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
+app.get('/timestamp/:date', function (req, res) {
+    var date = moment(req.params.date, "MMMM D, YYYY");
+    if(!date.isValid()) date = moment(req.params.date, "X");
+    if(date.isValid()) res.send({ unix: Number(date.format("X")), natural: date.format("MMMM D, YYYY")});
+    else res.send({ unix: null, natural: null});
+});
+
+var port = process.env.PORT || 8080;
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+  console.log('Timestamp Microservice app listening on port ' + port + '!');
 })
